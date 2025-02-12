bf1438cb28171b7b8670cffb200892adebb562e1
tsvbasa <tsvbasa7@gmail.com>
2025-02-12 16:12:20 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_food/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.manropeTextTheme(),
+      ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +10,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(14),
@@ -19,17 +24,32 @@ class HomePage extends StatelessWidget {
                       children: [
                         Text(
                           "Deliver to",
+                          style: TextStyle(
+                            // ignore: deprecated_member_use
+                            color: Colors.black.withOpacity(0.5),
+                            fontSize: 14,
+                          ),
                         ),
                         Row(
                           children: [
-                            Icon(
-                              Icons.location_on,
+                            SvgPicture.asset(
+                              "assets/icons/map.svg",
+                              height: 20,
+                              color: Colors.black.withOpacity(0.8),
                             ),
-                            Text(
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            const Text(
                               "New York, USA",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
                             ),
                             Icon(
                               Icons.arrow_drop_down,
+                              color: Colors.black45,
                             ),
                           ],
                         ),
@@ -37,9 +57,39 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                   Container(
-                    width: 60,
-                    height: 60,
-                    color: Colors.amber,
+                    width: 54,
+                    height: 54,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          'https://images.pexels.com/photos/3866555/pexels-photo-3866555.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+                        ),
+                      ),
+                    ),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        Positioned(
+                          top: -11,
+                          right: -11,
+                          child: Container(
+                            height: 22,
+                            width: 22,
+                            padding: const EdgeInsets.all(4.0),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.red,
+                              radius: 8,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ],
               ),
